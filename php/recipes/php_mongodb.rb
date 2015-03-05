e293e124a8f7299db29907886f7d6325ab105288
Ben Milleare <ben@milleare.com>
2015-03-05 13:28:03 +0000

    Update php_mongodb.rb

--- php/recipes/php_mongodb.rb
+++ php/recipes/php_mongodb.rb
@@ -1,18 +1,4 @@
 execute 'install_php_mongo_driver' do
-  command "pecl install mongo"
+  command "apt-get -y install php5-mongo"
   action :run
-end
-
-template 'mongo.ini' do
-  case node[:platform]
-  when 'centos','redhat','fedora','amazon'
-    path "/etc/php.d/mongo.ini"
-  when 'debian','ubuntu'
-    path "/etc/php5/apache2/conf.d/20-mongo.ini"
-  end
-  source 'mongo.ini.erb'
-  owner 'root'
-  group 'root'
-  mode 0644
-  notifies :restart, resources(:service => 'apache2')
 end
